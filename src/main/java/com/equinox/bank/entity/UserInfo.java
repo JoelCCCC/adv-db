055ec87bc06bf33a5e241c5fd1d0a3344b14b5e1
Joel Chan <joelchan@Joels-MacBook-Air.local>
2026-07-02 11:08:58 +0700

    fix authentication

--- src/main/java/com/equinox/bank/entity/UserInfo.java
+++ src/main/java/com/equinox/bank/entity/UserInfo.java
@@ -21,10 +21,13 @@ public class UserInfo {
     private String email;
     private String password;
     private String roles;
-    public String getEmail() { return email; } // Or getName() / getUsername() depending on your setup
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
-    public String getRoles() { return roles; }
-    
-    // Also ensure you have a setPassword method for when you encode it!
     public void setPassword(String password) { this.password = password; }
+    public String getRoles() { return roles; }
+    public void setRoles(String roles) { this.roles = roles; }
 }
